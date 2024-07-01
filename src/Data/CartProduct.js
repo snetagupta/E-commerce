c7d1604e1651a9f8b89c105a0a176fed990c0ac3
sneta gupta <snetagupta4nov@gmail.com>
2024-07-01 21:44:43 +0530

    add cart and register page

---
+++ src/Data/CartProduct.js
@@ -0,0 +1,29 @@
+const cartProducts = [{
+    image:"https://rukminim2.flixcart.com/image/224/224/xif0q/stuffed-toy/s/2/o/adorable-strawberry-rabbit-plushie-cute-bunny-soft-toy-35-rss-original-imahf7vvm3gazrzz.jpeg?q=90",
+    name:"Pidilite Fevicryl Acrylic Colours set- Sunflower Kit (10 Color)",
+    quantity:"Set of 10, Multicolor",
+    seller:'Seller:SURICYBCOMBazaar',
+    actualprice:"₹250",
+    price:'₹238',
+    discount:"4% Off",
+},
+{
+    image:"https://rukminim2.flixcart.com/image/224/224/xif0q/paint/d/i/n/10-150-fevicryl-acrylic-colours-set-sunflower-kit-10-colours-x-original-imahfyn7mpfpyzyy.jpeg?q=90",
+    name:"RSS SOFT TOYS Adorable Strawberry Rabbit Plushie, Cute Bun",
+    quantity:"Pink",
+    seller:'Seller:JANVI SOFT TOYS',
+    actualprice:"₹999",
+    price:'₹269',
+    discount:"73% Off",
+},
+{
+    image:"https://rukminim2.flixcart.com/image/224/224/xif0q/post-it/r/t/i/400-sheets-fluorescent-paper-self-adhesive-memo-pad-sticky-notes-original-imagwtzfywenfja9.jpeg?q=90",
+    name:"Nutshell Vibrant Colors Fluorescent Paper 400 Sheets 3 x 3",
+    quantity:"Set Of 1, Neon",
+    seller:'Seller:PACHIBA ENTERPRISE',
+    actualprice:"₹499",
+    price:'₹180',
+    discount:"63% Off",
+},
+]
+export default cartProducts
