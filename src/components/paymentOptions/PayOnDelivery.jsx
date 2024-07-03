02b4bee142df7b6fbee9693a62f8456a1f6062d1
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-03 22:57:36 +0530

    add payment options

---
+++ src/components/paymentOptions/PayOnDelivery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PayOnDelivery = () => {
+  return (
+    <div>PayOnDelivery</div>
+  )
+}
+
+export default PayOnDelivery
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -3,10 +3,17 @@ import upi from "../assets/upi.svg";
 import netBanking from "../assets/netBanking.svg";
 import paylater from "../assets/paylater.svg";
 import payOnDelivery from "../assets/payOnDelivery.svg";
-import pluxee from "../assets/pluxee.svg";
 import wallet from "../assets/wallet.svg";
+import { useState } from "react";
+import Card from "../components/paymentOptions/Card";
+import Upi from "../components/paymentOptions/Upi";
+import PayLater from "../components/paymentOptions/PayLater";
+import Wallet from "../components/paymentOptions/Wallet";
+import NetBanking from "../components/paymentOptions/NetBanking";
+import PayOnDelivery from "../components/paymentOptions/PayOnDelivery";
 
 const Payment = () => {
+  const [option, setOption] = useState(0);
   return (
     <>
       <div className="">
@@ -18,62 +25,85 @@ const Payment = () => {
         <div className="px-80 py-4 bg-gray-200">
           <p className="text-xl font-semibold py-4">Payment Methods</p>
           <div className="flex justify-center border w-full h-[80vh]">
-            <div className="bg-yellow-50 w-[30%] flex flex-col gap-6 p-4">
-              <div className="flex gap-4">
+            <div className="bg-yellow-50 w-[30%] flex flex-col gap-3 p-4">
+              <div
+                className={`flex gap-4 p-2 hover:bg-green-50 hover:rounded-md hover:border ${
+                  option === 0 && "border rounded-md bg-green-50"
+                } cursor-pointer`}
+                onClick={() => setOption(0)}
+              >
                 <img src={upi} />
                 <p className="text-lg font-medium">UPI</p>
               </div>
-              <div className="flex gap-4">
-                <img src={pluxee} />
-                <p className="text-lg font-medium">Pluxee</p>
-              </div>
-              <div className="flex items-center gap-4">
-                <AiTwotoneCreditCard className="text-xl"/>
+              <div
+                className={`flex items-center gap-4 p-2 transition-all duration-150 hover:bg-green-50 hover:rounded-md hover:border  ${
+                  option === 1 && "border rounded-md bg-green-50"
+                } cursor-pointer`}
+                onClick={() => setOption(1)}
+              >
+                <AiTwotoneCreditCard className="text-xl" />
                 <p className="text-lg font-medium">Credit/ Debit Card</p>
               </div>
-              <div className="flex gap-4">
+              <div
+                className={`flex gap-4 p-2 hover:bg-green-50 hover:rounded-md hover:border ${
+                  option === 2 && "border rounded-md bg-green-50"
+                } cursor-pointer`}
+                onClick={() => setOption(2)}
+              >
                 <img src={paylater} />
                 <p className="text-lg font-medium">PayLater</p>
               </div>
-              <div className="flex gap-4">
+              <div
+                className={`flex gap-4 p-2 hover:bg-green-50 hover:rounded-md hover:border ${
+                  option === 3 && "border rounded-md bg-green-50"
+                } cursor-pointer`}
+                onClick={() => setOption(3)}
+              >
                 <img src={wallet} />
                 <p className="text-lg font-medium">Wallets</p>
               </div>
-              <div className="flex gap-4">
+              <div
+                className={`flex gap-4 p-2 hover:bg-green-50 hover:rounded-md hover:border ${
+                  option === 4 && "border rounded-md bg-green-50"
+                } cursor-pointer`}
+                onClick={() => setOption(4)}
+              >
                 <img src={netBanking} />
                 <p className="text-lg font-medium">NetBanking</p>
               </div>
-              <div className="flex gap-4">
+              <div
+                className={`flex gap-4 p-2 hover:bg-green-50 hover:rounded-md hover:border ${
+                  option === 5 && "border rounded-md bg-green-50"
+                } cursor-pointer`}
+                onClick={() => setOption(5)}
+              >
                 <img src={payOnDelivery} />
-                <p className="text-lg font-medium">Pay On Delivrery</p>
+                <p className="text-lg font-medium">Pay On Delivery</p>
               </div>
-              
+
               <div className="flex justify-center mt-auto">
                 {/* <p>Secured by</p> */}
-                <img src="https://assets.juspay.in/hyper/images/common/jp_juspay_brand.png" className="w-[130px]"/>
+                <img
+                  src="https://assets.juspay.in/hyper/images/common/jp_juspay_brand.png"
+                  className="w-[130px]"
+                />
                 {/* <p className="text-lg font-medium">JUSTPAY</p> */}
               </div>
             </div>
             <div className="bg-white w-[70%] p-4">
-            <p className="text-xl font-medium text-gray-800 border-b">Enter Credit / Debit card details</p>
-                <div className="flex flex-col gap-4 w-[60%] py-4 px-2">
-             
-              <div className="flex flex-col gap-2">
-              <label className="text-green-500">Card Number</label>
-              <input type="text" placeholder="Enter card number" className="border border-green-500 py-3 px-2 rounded-md"/>
-              </div>
-              <div className="flex gap-4">
-              <div className="flex flex-col gap-2">
-              <label className="text-gray-600">Expiry</label>
-              <input type="text" placeholder="MM/YY"  className="border py-3 px-2 rounded-md"/>
-              </div>
-              <div className="flex flex-col gap-2">
-              <label className="text-gray-600">CVV</label>
-              <input type="text" placeholder="CVV"  className="border py-3 px-2 rounded-md"/>
-              </div>
-              </div>
-            <button className="text-white bg-green-600 rounded-md px-12 py-3">Proceed to Pay</button>
-            </div>
+              {option === 0 ? (
+                <Upi />
+              ) : option === 1 ? (
+                <Card />
+              ) : option === 2 ? (
+                <PayLater />
+              ) : option === 3 ? (
+                <Wallet />
+              ) : option === 4 ? (
+                <NetBanking />
+              ) : (
+                <PayOnDelivery />
+              )}
             </div>
           </div>
         </div>
